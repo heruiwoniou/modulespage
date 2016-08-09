d2c2ebbba40bd8841fbe5d52ee60d9b1cb766c35
heruiwoniou <heruiwoniou@qq.com>
2016-08-09 15:19:58 +0800

    '添加了自动计算leftrightcontrol[min-height]的js方法,参见teacher-manage.js'

--- _Runtime/Content/js/modules_base/teacher-manage.js
+++ _Runtime/Content/js/modules_base/teacher-manage.js
@@ -1,3 +1,7 @@
-require(['Static/js/application'],function(application){
+require(['Static/js/application','Static/js/common/client/WinResizeResponder'],function(application,Responder){
 	application.init();
+	var $control = $('#mybody')
+	Responder.resize(function(wh){
+		$control.css({minHeight:wh - 185 + 'px'})
+	})
 })
