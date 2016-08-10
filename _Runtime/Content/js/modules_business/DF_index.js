059a3e8191bb27e1ca9ce565b0269bc7a1806fac
dengfan <dengfan@qq.com>
2016-08-10 15:16:56 +0800

    修改组织框架.html

---
+++ _Runtime/Content/js/modules_business/DF_index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2016/8/10.
+ */
+define(function(){
+    return {
+        init:function(){
+            //初始化页面内容
+            //返回值true/false决定是否执行util.js下的_init_方法
+        }
+    }
+})
