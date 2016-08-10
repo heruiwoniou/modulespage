dd31e638aa5529f3321f4ead2b9218fa4a58a9ec
xiaohongmei <xiaohongmei@qq.com>
2016-08-10 16:35:25 +0800

    角色权限设置

--- _Runtime/Content/js/modules_business/Role-authorization.js
+++ _Runtime/Content/js/modules_business/Role-authorization.js
@@ -1,7 +1,12 @@
 define(function(){
     return {
         addList:function(){
-            $("#Plant-Names").append("<li class='Plant-Names-list'>名称" +
+            $("#Plant-Names").append("<li class='Plant-Names-list'>" +
+                "<input type='text' value='教务处老师' class='inputName'>"+
+                "<span class='compile'>"+
+                "<a href='javascriptp:;'><i class='icon-sure color-3'></i></a>"+
+                "<a href='javascript:;'><i class=' icon-cancel color-2'></i></a>"+
+                "</span>"+
                 "<span class='compile-edit'>"+
                 "<a href='javascriptp:;'><i class='icon-edit color-3'></i>修改</a>"+
                 "<a href='javascript:;' ><i class='icon-delete color-2'></i>删除</a>"+
---
+++ _Runtime/Content/js/modules_business/user-permissions.js
@@ -0,0 +1,9 @@
+define(function(){
+    return {
+
+        init:function(){
+            //初始化页面内容
+            //返回值true/false决定是否执行util.js下的_init_方法
+        }
+    }
+});
