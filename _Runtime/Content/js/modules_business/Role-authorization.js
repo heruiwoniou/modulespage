4ead3102a9768d23dd6eab723ccb84f4b1678fa0
xiaohongmei <xiaohongmei@qq.com>
2016-08-10 14:48:50 +0800

    角色权限设置

--- _Runtime/Content/js/modules_business/Role-authorization.js
+++ _Runtime/Content/js/modules_business/Role-authorization.js
@@ -1,7 +1,12 @@
 define(function(){
     return {
         addList:function(){
-            $("#Plant-Names").append("<li class='Plant-Names-list'>名称</li>")
+            $("#Plant-Names").append("<li class='Plant-Names-list'>名称" +
+                "<span class='compile-edit'>"+
+                "<a href='javascriptp:;'><i class='icon-edit color-3'></i>修改</a>"+
+                "<a href='javascript:;' ><i class='icon-delete color-2'></i>删除</a>"+
+                "</span>"+
+                "</li>")
         },
         init:function(){
             //初始化页面内容
