e4b019c33d4cdab20079d097d2d387acc6cb241c
dengfan <dengfan@qq.com>
2016-08-26 14:33:59 +0800

    Merge branch 'webportals-1' of herui:web-portals into webportals-1
    # Conflicts:
    #	_Runtime/Content/style/modules_base/Personnel-information.styl

--- _Runtime/Content/js/modules_base/data-center.js
+++ _Runtime/Content/js/modules_base/data-center.js
@@ -6,4 +6,19 @@ require([
 	 * [图片加载]
 	 */
 	$.loadImage();
+
+	$(".scroll-bar").scrollBar({
+		//theme:
+		//"dark",
+		//"inset-dark"
+		//"inset-2-dark",
+		//minimal-dark
+		//dark-3
+		//"dark-2",
+		scrollInertia: 400,
+		advanced:{ autoScrollOnFocus: false },
+		autoHideScrollbar:true,
+		scrollButtons:{enable:false},
+		advanced:{ updateOnImageLoad: false }
+	});
 })
--- _Runtime/Content/js/modules_business/work-quality.js
+++ _Runtime/Content/js/modules_business/work-quality.js
@@ -6,6 +6,7 @@ define(function(){
 			//初始化页面内容
 			//返回值true/false决定是否执行util.js下的_init_方法
 			$( ".date-begin" ).datepicker();
+
 		}
 	}
 })
