5ee883118ff12f1b54c453e2ebcbb3349bdd6ca1
heruiwoniou <heruiwoniou@qq.com>
2016-09-01 16:16:13 +0800

    'addcommunitybase样式'

--- _Runtime/Content/js/common/util.js
+++ _Runtime/Content/js/common/util.js
@@ -156,8 +156,18 @@ define(function(){
 			cache.src = src;
 			return deferred;
 		},
+		initControl: function(parent){
+        	var $parent = parent && parent.length != 0 ? $(parent) : $(document);
+        	$parent.find(':checkbox[cs-control]').CheckBoxInit();
+        	$parent.find(':radio[cs-control]').RadioBoxInit();
+        	$parent.find('select[cs-control]').SingleComboxInit();
+        	$parent.find('input[type*=text][cs-control*=ButtonTextBox],input[type*=password][cs-control*=ButtonTextBox]').ButtonTextBoxInit();
+            $parent.find('input[type*=text][cs-control*=DataPicker]').DataPickerInit();
+            $parent.find('input[cs-control][type*=file]').ProtoUploadInit();
+        },
 		init:function(){
 			//这里添加页面加载完成后公用的初始化功能
+			this.initControl();
 		}
 	}
 })
