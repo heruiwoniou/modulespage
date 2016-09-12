52d04d2f7ed49889832d46b1e0990a2c4ded75c0
dengfan <dengfan@qq.com>
2016-09-12 09:24:20 +0800

    Merge branch 'webportals-1' of herui:web-portals into webportals-1
    # Conflicts:
    #	_Runtime/my-research.html

--- _Runtime/Content/js/modules_base/Personnel-information.js
+++ _Runtime/Content/js/modules_base/Personnel-information.js
@@ -1,6 +1,7 @@
 require([
     'Static/js/application',
     'Static/js/libs/jquery.scrollbar/jquery.mCustomScrollbar'
+    // 'Static/js/libs/jquery.scrollbar/jquery.mCustomScrollbar.concat'
 ],function(application){
     application.init();
     /**
@@ -22,5 +23,18 @@ require([
         scrollButtons:{enable:false},
         advanced:{ updateOnImageLoad: false }
     });
+    /*点击滚动条*/
+    $(function(){
+        $('.information_nav ul li').click(function () {
+            var ind = $('.information_nav ul li').index(this) + 1;
+            $(".main").scrollBar('scrollTo', $('#float0' + ind),{callbacks:{onScroll:function(){
+                console.log(123);
+            }}});
+            $('.information_nav ul li').removeClass('active');
+            $(this).addClass('active');
+        });
+        $('.up_btn').click(function(){
+            $(".main").scrollBar('scrollTo', 0)});
+        })
 
 });
